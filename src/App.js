c12823f3497de32682ce60825f0ae7325892c866
Daniel Pinto <daniktrino90gmail.com>
2019-04-24 10:24:55 -0500

    Primera version

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <code>Lo odio </code>
+          </p>
+          <a
+            className="App-link"
+            href="https://www.youtube.com/channel/UC-lHJZR3Gqxm24_Vd_AJ5Yw  "
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          Clikeame i te buelbes omvre
+          </a>
+        </header> 
+      </div>
+    );
+  }
+}
+
+export default App;
